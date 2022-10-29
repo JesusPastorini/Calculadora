203657cb8c4bac22bad5e1863665678a0f88a9c4
Jesus <juniorpastorini_@hotmail.com>
2022-10-29 01:44:45 -0300

    digitosparatela

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,22 @@ class Calculator {
     }
 //manda os digitos para o visor    
 addDigit(digit){
-
+    this.currentOperation = digit
+    this.updateScreen()
+}
+//altera os valores da tela
+updateScreen(){
+    this.currentOperationText.innerText += this.currentOperation;
 }
 }
+const calc = new Calculator(previousOperationText, currentOperationText);
 //value recebe os valores texto do htmls
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText;
         //convertendo valor recebido string para numero +value
         if(+value >= 0 || value === "."){
-
+            calc.addDigit(value);
         }else{
 
         }
